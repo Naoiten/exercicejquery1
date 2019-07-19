f9f14c2c9291d35b72a1fe91058f3527c6020226
Antoine <antoine.hotiin32@laposte.net>
2019-07-19 16:53:16 +0200

    exercice finis

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+                    $('#firstText').hide();
+                    $('#secondText').hide();
+                    $('#thirdText').hide();
+                });
